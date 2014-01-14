6af7e334530174f2e61fd9d852321ec5e0f48d9e
avignal <ant.vignal@gmail.com>
2014-01-14 12:16:00 +0100

    Mod "one shot" satisfaction

--- JADE_SMA_Commercial/src/sma/tools/Config.java
+++ JADE_SMA_Commercial/src/sma/tools/Config.java
@@ -11,8 +11,8 @@ package sma.tools;
 public class Config {
 
 	public static final int TICKER_DELAY = 2000;
-	public static final int STOCK_MAX_PRODUCTION = 100;
-	public static final int STOCK_MAX_CONSUMPTION = 100;
+	public static final int STOCK_MAX_PRODUCTION = 50;
+	public static final int STOCK_MAX_CONSUMPTION = 50;
 	public static final float INIT_MONEY = 200;
 	public static final float INIT_PRICE = 1;
 	public static final double INFINI = 99999999;
