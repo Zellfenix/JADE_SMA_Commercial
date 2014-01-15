ab88e2f0b1f70489ee3bb853081fc548ca701d56
Cymoud <cymoud@gmail.com>
2014-01-15 10:25:59 +0100

    Graphiques actualisés

--- JADE_SMA_Commercial/src/sma/tools/analyse/Analyse.java
+++ JADE_SMA_Commercial/src/sma/tools/analyse/Analyse.java
@@ -26,18 +26,18 @@ public class Analyse extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private static Analyse instance;
 	
-	protected HashMap<String, AgentRepresentation> agents;
+	//protected HashMap<String, AgentRepresentation> agents;
 	protected HashMap<String, Stats> agent_stats;
 	protected HashMap<String, AgentChart> agent_chart;
 	
 	private JTabbedPane tabbedPane;
 	private JPanel panel_stats;
-	private JPanel panel_graphe;
+	//private JPanel panel_graphe;
 	private JPanel panel_charts;
 	private TableModelDynamique tableModel;
 
 	private int count = 0;
-
+	private long last_update = System.currentTimeMillis();
 
 	public Analyse() throws HeadlessException {
 		super();
@@ -52,7 +52,7 @@ public class Analyse extends JFrame {
 	}
 	
 	private void init(){
-		agents = new HashMap<String, AgentRepresentation>(2);
+		//agents = new HashMap<String, AgentRepresentation>(2);
 		agent_stats = new HashMap<String, Stats>(2);
 		agent_chart = new HashMap<String, AgentChart>(2);
 		
@@ -65,64 +65,62 @@ public class Analyse extends JFrame {
 		panel_stats.add(jtable.getTableHeader(), BorderLayout.NORTH);
 		panel_stats.add(jtable, BorderLayout.CENTER);
 		
-		panel_graphe = new JPanel();
-		panel_graphe.setLayout(new CircleLayout());
+		//panel_graphe = new JPanel();
+		//panel_graphe.setLayout(new CircleLayout());
 
 		panel_charts = new JPanel();
 		//panel_chart.setLayout(new CircleLayout());
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		tabbedPane.addTab("Graphe", panel_graphe);
+		//tabbedPane.addTab("Graphe", panel_graphe);
 		tabbedPane.addTab("Statistiques", panel_stats);
 		tabbedPane.addTab("Graphiques", new JScrollPane(panel_charts));
 		getContentPane().add(tabbedPane);
 		setSize(1024, 700);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//pack();
 		setVisible(true);
-		//setExtendedState(JFrame.MAXIMIZED_BOTH);
 	}
 	
 	public void agent_setup(AgentCommercial agentCommercial){
-		AgentRepresentation agentRepresentation = new AgentRepresentation();
-		panel_graphe.add(agentRepresentation);
-		agents.put(agentCommercial.getName(), agentRepresentation);
+		//AgentRepresentation agentRepresentation = new AgentRepresentation();
+		//panel_graphe.add(agentRepresentation);
+		//agents.put(agentCommercial.getName(), agentRepresentation);
 		
 		agent_stats.put(agentCommercial.getName(), new Stats(agentCommercial));
 		tableModel.setStats( new ArrayList<Stats>(agent_stats.values()) );
 		
-		panel_graphe.setSize(getPreferredSize().width, getPreferredSize().height);
-		pack();
+		//panel_graphe.setSize(getPreferredSize().width, getPreferredSize().height);
+		//pack();
 	}
 	
 	public void agent_update(AgentCommercial agentCommercial){
 		agent_stats.get(agentCommercial.getName()).update(agentCommercial);
 		tableModel.fireTableDataChanged();
-		agents.get(agentCommercial.getName()).update(agentCommercial);
+		//agents.get(agentCommercial.getName()).update(agentCommercial);
 		agent_stats.get(agentCommercial.getName()).setStatus("Alive");
 
-		for (Entry<String, Stats> e : Analyse.getInstance().agent_stats.entrySet()) {
-			if (!agent_chart.containsKey(e.getKey())) {
-				AgentChart chart = new AgentChart(e.getKey());
-				agent_chart.put(e.getKey(), chart);
-				panel_charts.add(chart);
+		if (System.currentTimeMillis() - last_update >= 500) {
+			for (Entry<String, Stats> e : Analyse.getInstance().agent_stats.entrySet()) {
+				if (!agent_chart.containsKey(e.getKey())) {
+					AgentChart chart = new AgentChart(e.getKey());
+					agent_chart.put(e.getKey(), chart);
+					panel_charts.add(chart);
+				}
+				agent_chart.get(e.getKey()).update(count, e.getValue());
 			}
-			agent_chart.get(e.getKey()).update(count, e.getValue());
+			count++;
+			panel_charts.getParent().invalidate();
+			panel_charts.getParent().revalidate();
+			panel_charts.getParent().repaint();
+			last_update = System.currentTimeMillis();
 		}
-		count++;
-		tabbedPane.invalidate();
-		tabbedPane.revalidate();
-		tabbedPane.updateUI();
-		tabbedPane.repaint();
-		panel_charts.invalidate();
-		panel_charts.revalidate();
-		panel_charts.updateUI();
-		panel_charts.repaint();
 	}
 	
 	public void agent_dead(AgentCommercial agentCommercial){
 		agent_stats.get(agentCommercial.getName()).update(agentCommercial);
 		tableModel.fireTableDataChanged();
-		agents.get(agentCommercial.getName()).update(agentCommercial);
+		//agents.get(agentCommercial.getName()).update(agentCommercial);
 		agent_stats.get(agentCommercial.getName()).setStatus("Dead");
 	}
 
